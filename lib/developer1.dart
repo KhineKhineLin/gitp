95b7a2ae6e2b6f1ef370643f0e03ca3ef10833a0
Khine Lin <khinetun2811@gmail.com>
2023-07-10 23:09:41 +0630

    test2

---
+++ lib/developer1.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Developer1 extends StatefulWidget {
+  const Developer1({super.key});
+
+  @override
+  State<Developer1> createState() => _Developer1State();
+}
+
+class _Developer1State extends State<Developer1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Developer 1'),
+      ),
+    );
+  }
+}
